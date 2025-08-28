5bfab1878cebc0334b58dea2aa3831e7a99827c3
Tim Burns <92951498+OpenSource-For-Freedom@users.noreply.github.com>
2025-08-28 01:20:05 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,11 @@ if [[ "$(id -u)" -eq 0 ]]; then
   /usr/local/bin/rhel.hardn.sh || echo "WARN: hardening script completed with warnings"
   install -d -o hardn -g hardn /opt/hardn-xdr/state
   exec su -s /bin/bash -c "$*" hardn
+fi
+
+
+if [[ $# -eq 0 ]]; then
+  tail -f /dev/null
 else
   exec "$@"
 fi
